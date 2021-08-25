be5c66eebb20804e2b4d6d86fc3e3139fe28e367
Shadab Ali <shadabali604@gmail.com>
2021-08-25 23:05:43 +0530

    request folder created

---
+++ components/Nav.jsx
@@ -0,0 +1,10 @@
+const Nav = () => {
+    return <nav>
+        <div>
+            
+      </div>
+        
+  </nav>;
+};
+
+export default Nav;
--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,16 @@ import Head from "next/head";
 import Image from "next/image";
 import Header from "../components/Header";
 import styles from "../styles/Home.module.css";
+import Nav from "../components/Nav";
 
 export default function Home() {
   return (
     <>
-      <Header />
       <Head>
         <title>Hulu 2.0</title>
       </Head>
+      <Header />
+      <Nav />
       <h1 className="text-blue-900 text-3xl text-center">This is Home Page</h1>
     </>
   );
