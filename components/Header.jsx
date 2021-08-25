cbb8a047d377dcfc5615d055012e452132dbfca3
Shadab Ali <shadabali604@gmail.com>
2021-08-25 22:59:25 +0530

    header item added

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,18 +1,38 @@
 import Image from "next/image";
 import HeaderItem from "./HeaderItem";
+import {
+  BadgeCheckIcon,
+  CollectionIcon,
+  HomeIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
 
 const Header = () => {
   return (
-    <header className="">
-      <div>
-        <HeaderItem title="Home" icon="I" />
+    <header
+      className="flex flex-col sm:flex-row m-5
+      justify-between
+      items-center
+      h-auto"
+    >
+      <div className="flex flex-grow justify-evenly max-w-2xl text-center">
+        <HeaderItem title="Home" Icon={HomeIcon} />
+        <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
+        <HeaderItem title="SEARCH" Icon={SearchIcon} />
+        <HeaderItem title="USER" Icon={UserIcon} />
+        <HeaderItem title="COLLECTION" Icon={CollectionIcon} />
+        <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} />
       </div>
-      <Image
-        src="https://links.papareact.com/ua6"
-        width={200}
-        height={100}
-        alt=""
-      />
+      <div className="text-center">
+        <Image
+          src="https://links.papareact.com/ua6"
+          width={200}
+          height={100}
+          alt=""
+        />
+      </div>
     </header>
   );
 };
--- components/HeaderItem.jsx
+++ components/HeaderItem.jsx
@@ -1,3 +1,12 @@
-const HeaderItem = ({ title, icon }) => {};
+const HeaderItem = ({ title, Icon }) => {
+  return (
+    <div className="group flex flex-col cursor-pointer w-12 text-gray-300 hover:text-white text-center">
+      <Icon className="h-8 mb-1 group-hover:animate-bounce" />
+      <p className="opacity-0 tracking-widest group-hover:opacity-100">
+        {title}
+      </p>
+    </div>
+  );
+};
 
 export default HeaderItem;
